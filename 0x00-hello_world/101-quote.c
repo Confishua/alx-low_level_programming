49ede783ce734f94de9fbd69f373cc58ac1e0098
Confidence Ozioyiza Daniel <ekehioboro@gmail.com>
2023-02-17 09:59:05 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 /*
 * Main - prints letters
 *
-* Return: 0
+* Return: 1
 */
 int main(void)
 {
 printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.\n");
-return (0);
+return (1);
 }
