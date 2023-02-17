8c41cd6a1d5b2ffe1c2f7cb76c0639041762ede3
Confidence Ozioyiza Daniel <ekehioboro@gmail.com>
2023-02-17 09:56:12 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-
 /*
-* main - gives the accurate command
-* 
-* Return: displays 1
+* Main - prints letters
+*
+* Return: 0
 */
-
 int main(void)
 {
-fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-return (1);
+printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.\n");
+return (0);
 }
