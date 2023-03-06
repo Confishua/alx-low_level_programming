1ae14fcf83115da86cee55f86825f267d77c332e
Confidence Ozioyiza Daniel <ekehioboro@gmail.com>
2023-03-05 18:07:59 -0800

    a piece of strings printed to the stderr using fprintf

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,6 +9,6 @@
 int main(void)
 
 {
-printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.");
+  fprintf(stderr, "%s", "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 return (1);
 }
