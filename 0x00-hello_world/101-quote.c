6ead26bc2334025d6e870368ac4517c9bf4a65e3
Confidence Ozioyiza Daniel <ekehioboro@gmail.com>
2023-03-05 18:41:15 -0800

    a piece of strings printed to the stderr using fprintf

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-
 /*
-* Main - a script that prints to the stderr
+* main- a script that prints to the stderr
 * Return: 1
 **/
 
 int main(void)
-/* to print to stderr */
 {
 fprintf(stderr, "%s", "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 return (1);
