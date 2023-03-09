95a3fde7d25c4d5abb341855c4faa487411c2c9e
Confidence Ozioyiza Daniel <ekehioboro@gmail.com>
2023-03-09 05:18:13 -0800

    printing the length of a string

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,16 +1,11 @@
 #include "main.h"
 
 /**
-
- * _strlen_recursion - Returns the length of a string.
-
- * @s: The string to be measured.
-
- *
-
- * Return: The length of the string.
-
- */
+* _strlen_recursion - Returns the length of a string.
+* @s: The string to be measured.
+*
+* Return: The length of the string.
+*/
 
 int _strlen_recursion(char *s)
 
@@ -30,6 +25,6 @@ longit += _strlen_recursion(s + 1);
 }
 
 
-    return (longit);
+return (longit);
 
 }
