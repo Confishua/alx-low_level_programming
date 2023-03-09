73d50f3373ce23b23fb9c47543c9960e7240bcf2
Confidence Ozioyiza Daniel <ekehioboro@gmail.com>
2023-03-09 07:43:14 -0800

    Debugging

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,12 +2,11 @@
 
 /**
 * mul - multiply 2 int
-* a: first int (this is serves as the first integer)
-* b: second int (the second integer is her)
-* Return: addition of the multiplicattion
+*Return: addition of the multiplicattion
 */
 
-int mul(int a, int b)
+int mul(int a, int b);
 {
+  int mul(a * b);
 return (a * b);
 }
---
+++ README.md
@@ -0,0 +1,1 @@
+C programming is a middle level language
