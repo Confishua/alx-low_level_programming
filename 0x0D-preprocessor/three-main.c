443abb902c992dd0fe85ef0bf2eac8aa0b4dc2c8
Confidence Ozioyiza Daniel <ekehioboro@gmail.com>
2023-03-20 16:20:07 -0700

    The absolute value of a number

--- 0x0D-preprocessor/three-main.c
+++ 0x0D-preprocessor/three-main.c
@@ -12,8 +12,9 @@ int main(void)
     int i;
     int j;
 
-    i = ABS((-98) * 10);
-    j = ABS((98) * 10);
-    printf("%d, %d\n", i, j);
-    return (0);
+    i = ABS(-98) * 10;
+j = ABS(98) * 10;
+
+printf("%d, %d\n", i, j);
+return (0);
 }
