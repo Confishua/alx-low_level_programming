c1f05bd74988850c326083e5057c3f37c0cff322
Confidence Ozioyiza Daniel <ekehioboro@gmail.com>
2023-03-20 13:21:18 -0700

    a program that print the absolute value of a number

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,15 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+/**
+* main - function to print an absolute value
+* ABS - abso1lute number
+* N - absolute number
+* return: @N
+*/
+
+int ABS(int N)
+{
+return ((N < 0) ? (-N) : (N));
+}
+#endif
