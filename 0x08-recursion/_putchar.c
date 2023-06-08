396579f539ad9ea2ba2d1ffeb274feb6ea7625fe
root <danielconfidence@gmail.com>
2023-06-08 09:45:53 -0700

    This function prints strings

---
+++ 0x08-recursion/_putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* _putchar - writes the character to stdout
+* @c : The character to be displayed
+* Return: Always 0 for success
+*/
+
+int _putchar(*c)
+{
+return(write(1, &c, 1));
+}
