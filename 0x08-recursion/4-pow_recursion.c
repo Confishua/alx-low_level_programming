9dd24050facc601e19922bc86a8ebf97c43ce2fc
root <danielconfidence@gmail.com>
2023-06-18 10:49:29 -0700

    a task on recursion that returns the power of a number

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,21 +1,24 @@
 #include "main.h"
 
-
 /**
-*_pow_recursion - returns the power of any given numbers
+* _pow_recursion - return pow(x, y)
 * @x : x is the base number
 * @y: y is the power number
-* Return : 0 for success and
-*-1:  on error
+* Return: -1 on error or zero
 */
 
-int _pow_recursion(int x, int y) {
-    if (y == 0) {
-        return 1;
-    } else if (y < 0) {
-      return -1;
-	  } else {
-        return x * _pow_recursion(x, y - 1);
-    }
+int _pow_recursion(int x, int y)
+{
+if (y == 0)
+{
+return (1);
 }
-
+else if (y < 0)
+{
+return (-1);
+}
+else
+{
+return (x * _pow_recursion(x, y - 1));
+}
+}
