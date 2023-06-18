73493df03afee808bfbb0832216278606fae2ead
Confishua <ekehioboro@gmail.com>
2023-06-18 19:59:40 -0100

    tasks on recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -6,37 +6,19 @@
 * Return: 0 0n success, -1 on error
 */
 
-
-int calculate_sqrt(int n, int start, int end);
-int _sqrt_recursion(int n) {
-    if (n == 0 || n == 1) {
-        return n;
-    }
-    if (n % 2 != 0)
-      {
-	return (-1);
-      }
-    else {
-        return calculate_sqrt(n, 0, n);
-    }
-}
-
-int calculate_sqrt(int n, int start, int end)
+int squareRootRecursion(int n, int guess)
 {
-  int mid;
-  int square;
-    if (start > end) {
-        return end;
-    }
-
-   mid = (start + end) / 2;
-   square = mid * mid;
-
-    if (square == n) {
-        return mid;
-    } else if (square > n) {
-        return calculate_sqrt(n, start, mid - 1);
-    } else {
-        return calculate_sqrt(n, mid + 1, end);
-    }
+if (guess * guess == n)
+{
+return (guess);
+}
+if (guess * guess > n)
+{
+return (-1);
+}
+return squareRootRecursion(n, guess + 1);
+}
+int _sqrt_recursion(int n)
+{
+return squareRootRecursion(n, 1);
 }
